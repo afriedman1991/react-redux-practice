b404c7aad210a1f17d858d313e4b7045561df596
unknown <afriedman1991@gmail.com>
2018-01-23 20:03:53 -0500

    first commit

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+imoprt React, {PropTypes} from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+    return (
+        <nav>
+            <IndexLink to="/"
+            activeClassName="active">Home</IndexLink>
+            {" | "}
+            <Link to="/cats" activeClassName="active">Cats</Link>
+        </nav>
+    );
+};
+
+export default Header;
